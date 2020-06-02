4cbd5808a9efcb1181fad2589e72261e9e69f929
Jacob <51512690+jac0b-w@users.noreply.github.com>
2020-06-03 00:41:59 +0100

    themes added

---
+++ themes.py
@@ -0,0 +1,65 @@
+themes = {
+    "dark":{
+        "settings_window":'''
+            QLineEdit  {
+                border-radius: 10px;
+                border: 1px solid white;
+                width: 150;
+                height: 20px;
+                background: #6f6f76;
+                color: white;
+                padding-left: 6px;
+                padding-right: 6px;
+            }
+            QLabel
+            {
+                color: white;
+            }
+            QRadioButton
+            {
+                color:white;
+            }
+            QPushButton
+            {
+                border-style: outset;
+                border-width: 2px;
+                border-radius: 15px;
+                border-color: white;
+                color: white;
+                padding: 4px;
+            }
+            QSpinBox
+            {
+                border-radius: 10px;
+                border: 1px solid white;
+                width: 150;
+                height: 20px;
+                background: #6f6f76;
+                color: white;
+                padding-left: 6px;
+            }
+            QDoubleSpinBox
+            {
+                border-radius: 10px;
+                border: 1px solid white;
+                width: 150;
+                height: 20px;
+                background: #6f6f76;
+                color: white;
+                padding-left: 6px;
+            }
+            QWidget
+            {
+                background: black;
+                font-size: 15px;
+            }
+        ''',
+        "menu":'''
+            QMenu
+            {
+                background-color:black;
+                color: white;
+            }
+        '''
+    }
+}
