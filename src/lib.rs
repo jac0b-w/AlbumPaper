70cdc38a16669876597635a6f20955bdbe96a4e9
jac0b-w <51512690+jac0b-w@users.noreply.github.com>
2021-07-30 23:28:42 +0100

    improve formatting

--- src/lib.rs
+++ src/lib.rs
@@ -5,14 +5,13 @@ use pyo3::{prelude::*, types::PyBytes};
 
 // Define module
 #[pymodule]
-fn albumpaper_imagegen(_py: Python, module: &PyModule) -> PyResult<()>  {
+fn albumpaper_imagegen(_py: Python, module: &PyModule) -> PyResult<()> {
     module.add_function(wrap_pyfunction!(linear_gradient, module)?)?;
     module.add_function(wrap_pyfunction!(radial_gradient, module)?)?;
     Ok(())
 }
 
-
-const CHUNK_SIZE: usize = 128*128;
+const CHUNK_SIZE: usize = 128 * 128;
 
 /// Returns the raw bytes of a linear gradient image
 ///
@@ -33,9 +32,9 @@ const CHUNK_SIZE: usize = 128*128;
 ///     [255, 0, 0],
 ///     [0, 0, 255]
 /// )
-/// 
+///
 /// image =  Image.frombuffer('RGB', [1920, 1080], raw)
-/// 
+///
 /// ```
 #[pyfunction]
 pub fn linear_gradient(
@@ -46,26 +45,26 @@ pub fn linear_gradient(
 ) -> PyObject {
     let mut background: RgbImage = ImageBuffer::new(geometry.0 as u32, geometry.1 as u32);
 
-    let max_dist = (geometry.0 + geometry.1) as f64; 
+    let max_dist = (geometry.0 + geometry.1) as f64;
 
     background
-        .par_chunks_mut(CHUNK_SIZE*3)
+        .par_chunks_mut(CHUNK_SIZE * 3)
         .enumerate()
         .for_each(|(chunk_num, block)| {
             for (pixel_num, pixel) in block.chunks_exact_mut(3).enumerate() {
-                let x_dist = (chunk_num*CHUNK_SIZE + pixel_num) as i32 % geometry.0;
-                let y_dist = (chunk_num*CHUNK_SIZE + pixel_num) as i32 / geometry.0;
+                let x_dist = (chunk_num * CHUNK_SIZE + pixel_num) as i32 % geometry.0;
+                let y_dist = (chunk_num * CHUNK_SIZE + pixel_num) as i32 / geometry.0;
                 let scaled_dist = (x_dist + y_dist) as f64 / max_dist;
 
                 for (i, subpix) in pixel.iter_mut().enumerate() {
-                    *subpix = ((to_color[i] as f64 * scaled_dist) +
-                        (from_color[i] as f64 * (1.0 - scaled_dist))) as u8;
+                    *subpix = ((to_color[i] as f64 * scaled_dist)
+                        + (from_color[i] as f64 * (1.0 - scaled_dist)))
+                        as u8;
                 }
             }
         });
     PyBytes::new(py, background.as_raw()).into()
 }
-
 
 /// Returns the raw bytes of a radial gradient image
 ///
@@ -86,9 +85,9 @@ pub fn linear_gradient(
 ///     [255, 0, 0],
 ///     [0, 0, 255]
 /// )
-/// 
+///
 /// image =  Image.frombuffer('RGB', [1920, 1080], raw)
-/// 
+///
 /// ```
 #[pyfunction]
 pub fn radial_gradient(
@@ -104,20 +103,25 @@ pub fn radial_gradient(
 
     // The background will adapt to the foreground size so that the inner_color will be at the edges of the art
     // and not just at the centre of the image
-    let max_dist = distance((geometry.0/2) as i32, (geometry.1/2) as i32) - (foreground_size/2) as f64;
+    let max_dist =
+        distance((geometry.0 / 2) as i32, (geometry.1 / 2) as i32) - (foreground_size / 2) as f64;
 
     background
-        .par_chunks_mut(CHUNK_SIZE*3)
+        .par_chunks_mut(CHUNK_SIZE * 3)
         .enumerate()
         .for_each(|(chunk_num, block)| {
             for (pixel_num, pixel) in block.chunks_exact_mut(3).enumerate() {
-                let x_dist = ((chunk_num*CHUNK_SIZE + pixel_num) as i32 % geometry.0) - (geometry.0/2);
-                let y_dist = ((chunk_num*CHUNK_SIZE + pixel_num) as i32 / geometry.0) - (geometry.1/2);
-                let scaled_dist = (distance(x_dist, y_dist) - (foreground_size/2) as f64) / max_dist;
+                let x_dist =
+                    ((chunk_num * CHUNK_SIZE + pixel_num) as i32 % geometry.0) - (geometry.0 / 2);
+                let y_dist =
+                    ((chunk_num * CHUNK_SIZE + pixel_num) as i32 / geometry.0) - (geometry.1 / 2);
+                let scaled_dist =
+                    (distance(x_dist, y_dist) - (foreground_size / 2) as f64) / max_dist;
 
                 for (i, subpix) in pixel.iter_mut().enumerate() {
-                    *subpix = ((outer_color[i] as f64 * scaled_dist) +
-                    (inner_color[i] as f64 * (1.0 - scaled_dist))) as u8
+                    *subpix = ((outer_color[i] as f64 * scaled_dist)
+                        + (inner_color[i] as f64 * (1.0 - scaled_dist)))
+                        as u8
                 }
             }
         });
